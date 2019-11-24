e7fc9c36590cf4584cd1666f2e8fafa9d4eb6846
DaniiNyan <daniela.araujo@ilegra.com>
2019-11-24 17:34:42 -0300

    Add more test and main

--- src/main/java/com/daniinyan/poc/simplesnacional/App.java
+++ src/main/java/com/daniinyan/poc/simplesnacional/App.java
@@ -1,7 +1,32 @@
 package com.daniinyan.poc.simplesnacional;
 
+import com.daniinyan.poc.simplesnacional.domain.Company;
+import com.daniinyan.poc.simplesnacional.repository.Attachment;
+import com.daniinyan.poc.simplesnacional.repository.AttachmentFive;
+import com.daniinyan.poc.simplesnacional.service.SimplesNacionalService;
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
 
+        Attachment attachmentFive = new AttachmentFive();
+        List<Company> companies = new ArrayList<>();
+        Company companyOne = new Company(attachmentFive, 30_000D, 200_000D);
+        Company companyTwo = new Company(attachmentFive, 100_000D, 750_000D);
+        Company companyThree = new Company(attachmentFive, 500_000D, 4_000_000D);
+        companies.add(companyOne);
+        companies.add(companyTwo);
+        companies.add(companyThree);
+
+        SimplesNacionalService simplesNacionalService = new SimplesNacionalService();
+
+        Observable.fromIterable(companies)
+                .map(company -> simplesNacionalService.calculate(companies))
+                .map(Map::values)
+                .subscribe(System.out::println);
     }
 }
--- src/test/java/com/daniinyan/poc/simplesnacional/SimplesNacionalServiceTest.java
+++ src/test/java/com/daniinyan/poc/simplesnacional/SimplesNacionalServiceTest.java
@@ -7,6 +7,7 @@ import com.daniinyan.poc.simplesnacional.service.SimplesNacionalService;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -24,5 +25,24 @@ public class SimplesNacionalServiceTest {
         SimplesNacionalService simplesNacionalService = new SimplesNacionalService();
         Map<Company, Double> result = simplesNacionalService.calculate(companies);
         assertEquals(4725D, result.get(company));
+    }
+
+    @Test
+    public void shouldCalculateMultipleCompanies() {
+        Attachment attachmentFive = new AttachmentFive();
+        List<Company> companies = new ArrayList<>();
+        Company companyOne = new Company(attachmentFive, 30_000D, 200_000D);
+        Company companyTwo = new Company(attachmentFive, 100_000D, 750_000D);
+        Company companyThree = new Company(attachmentFive, 500_000D, 4_000_000D);
+        companies.add(companyOne);
+        companies.add(companyTwo);
+        companies.add(companyThree);
+
+        SimplesNacionalService simplesNacionalService = new SimplesNacionalService();
+        Map<Company, Double> result = simplesNacionalService.calculate(companies);
+
+        assertEquals(4725D, result.get(companyOne));
+        assertEquals(18220D, result.get(companyTwo));
+        assertEquals(85000D, result.get(companyThree));
     }
 }
